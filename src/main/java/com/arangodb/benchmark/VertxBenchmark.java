68dfa134367d54436eeac2c1e13c667db9e88047
Michele Rastelli <michele@arangodb.com>
2022-10-13 13:35:52 +0200

    vertx refactoring with no verticle

--- src/main/java/com/arangodb/benchmark/VertxBenchmark.java
+++ src/main/java/com/arangodb/benchmark/VertxBenchmark.java
@@ -1,28 +1,67 @@
 package com.arangodb.benchmark;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpVersion;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VertxBenchmark extends AbstractBenchmark {
 
     private final int nThreads = ASYNC_THREADS;
     private final int maxPendingRequestsPerThread = MAX_PENDING_REQS_PER_THREAD;
-    private final Vertx vertx = Vertx.vertx();
+    private final List<Vertx> vertxes = new ArrayList<>();
+    private final List<WebClient> clients = new ArrayList<>();
     private final HttpProtocolVersion httpVersion;
+    private final HttpVersion protocol;
+
 
     public VertxBenchmark(HttpProtocolVersion httpVersion) {
         this.httpVersion = httpVersion;
+        switch (httpVersion) {
+            case HTTP11:
+                protocol = HttpVersion.HTTP_1_1;
+                break;
+            case H2C:
+                protocol = HttpVersion.HTTP_2;
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
+
+
     }
 
     @Override
     protected void start() {
         for (int i = 0; i < nThreads; i++) {
-            vertx.deployVerticle(new VertxClient(maxPendingRequestsPerThread, this, httpVersion));
+            Vertx vertx = Vertx.vertx(new VertxOptions().setEventLoopPoolSize(1));
+            int finalI = i;
+            vertx.runOnContext(e -> Thread.currentThread().setName("adb-eventloop-" + finalI));
+            vertxes.add(vertx);
+            WebClient client = WebClient.create(vertx, new WebClientOptions()
+                    .setKeepAlive(true)
+                    .setTcpKeepAlive(true)
+                    .setPipelining(true)
+                    .setReuseAddress(true)
+                    .setReusePort(true)
+                    .setHttp2ClearTextUpgrade(false)
+                    .setProtocolVersion(protocol)
+                    .setDefaultHost(AbstractBenchmark.HOST)
+                    .setDefaultPort(AbstractBenchmark.PORT)
+            );
+            clients.add(client);
+            new VertxClient(maxPendingRequestsPerThread, this, client);
         }
     }
 
     @Override
     protected void shutdown() {
-        vertx.close().toCompletionStage().toCompletableFuture().join();
+        clients.forEach(WebClient::close);
+        vertxes.forEach(it -> it.close().toCompletionStage().toCompletableFuture().join());
     }
 
     @Override
--- src/main/java/com/arangodb/benchmark/VertxClient.java
+++ src/main/java/com/arangodb/benchmark/VertxClient.java
@@ -1,37 +1,24 @@
 package com.arangodb.benchmark;
 
-import io.vertx.core.AbstractVerticle;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
-import io.vertx.core.http.HttpVersion;
 import io.vertx.ext.auth.authentication.UsernamePasswordCredentials;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.client.WebClientOptions;
 
-public class VertxClient extends AbstractVerticle {
+public class VertxClient {
 
-    private final HttpVersion protocol;
-    private final int maxPendingRequestsPerThread;
     private final AbstractBenchmark benchmark;
-    private WebClient client;
+    private final WebClient client;
     private final UsernamePasswordCredentials auth;
 
-    public VertxClient(int maxPendingRequestsPerThread, AbstractBenchmark benchmark, HttpProtocolVersion httpVersion) {
-        this.maxPendingRequestsPerThread = maxPendingRequestsPerThread;
+    public VertxClient(int maxPendingRequestsPerThread, AbstractBenchmark benchmark, WebClient client) {
         this.benchmark = benchmark;
+        this.client = client;
         auth = new UsernamePasswordCredentials(AbstractBenchmark.USER, AbstractBenchmark.PASSWD);
-
-        switch (httpVersion) {
-            case HTTP11:
-                protocol = HttpVersion.HTTP_1_1;
-                break;
-            case H2C:
-                protocol = HttpVersion.HTTP_2;
-                break;
-            default:
-                throw new IllegalArgumentException();
+        for (int i = 0; i < maxPendingRequestsPerThread; i++) {
+            sendReq();
         }
     }
 
@@ -51,24 +38,6 @@ public class VertxClient extends AbstractVerticle {
                 .get(AbstractBenchmark.PATH)
                 .authentication(auth)
                 .send(createCb());
-    }
-
-    @Override
-    public void start() {
-        client = WebClient.create(vertx, new WebClientOptions()
-                .setKeepAlive(true)
-                .setTcpKeepAlive(true)
-                .setPipelining(true)
-                .setReuseAddress(true)
-                .setReusePort(true)
-                .setHttp2ClearTextUpgrade(false)
-                .setProtocolVersion(protocol)
-                .setDefaultHost(AbstractBenchmark.HOST)
-                .setDefaultPort(AbstractBenchmark.PORT)
-        );
-        for (int i = 0; i < maxPendingRequestsPerThread; i++) {
-            sendReq();
-        }
     }
 
 }
